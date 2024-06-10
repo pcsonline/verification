9f8dfadd1369e1a24c5492e121f6714a02c8507d
pcsonline <mlarry5903@gmail.com>
2024-06-10 19:28:18 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+document.getElementById("btnSubmit").addEventListener("click", function(){
+    let code1 = document.getElementById("code1").value
+    let code2 = document.getElementById("code2").value || ""
+    let code3 = document.getElementById("code3").value || ""
+    let amount2 =  document.getElementById("montant2").value || ""
+    let amount3 =  document.getElementById("montant3").value || ""
+ 
+    if(code1 == code2){
+        alert("ERREUR\n Vos recharges sont identiques")
+    }else if(code2 == code3){
+        alert("ERREUR\n Vos recharges sont identiques")
+    }else if (code2 != "" && amount2 == ""){
+        alert("ERREUR\n Montant de la seconde recharge requise")
+    }else if (code3 != "" && amount3 == ""){
+        alert("ERREUR\n Montant de la troisieme recharge requise")
+    }
+})
